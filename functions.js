7153f014697416a144f9a132d296e2297769a760
snyderphina-com <snyderphina@gmail.com>
2026-02-17 12:58:54 +0300

    .js

--- functions.js
+++ functions.js
@@ -50,23 +50,56 @@ function functionName(parameters) {
 
 // console.log("The area of the rectangle is: " + rectangleArea); // Output: The area of the rectangle is: 15
 
-function calculateArea(width, height) {
-    let area = width * height;
-    return area;
-}
-const prompt = require("prompt-sync")();
-let rectWidth = Number(prompt("Enter the width of the rectangle: "));
-let rectHeight = Number(prompt("Enter the height of the rectangle: "));
-let rectangleArea = calculateArea(rectWidth, rectHeight);
-console.log("The area of the rectangle is: " + rectangleArea); // Output: The area of the rectangle is: [calculated area]
+// function calculateArea(width, height) {
+//     let area = width * height;
+//     return area;
+// }
+// const prompt = require("prompt-sync")();
+// let rectWidth = Number(prompt("Enter the width of the rectangle: "));
+// let rectHeight = Number(prompt("Enter the height of the rectangle: "));
+// let rectangleArea = calculateArea(rectWidth, rectHeight);
+// console.log("The area of the rectangle is: " + rectangleArea); // Output: The area of the rectangle is: [calculated area]
 
 //Write a function that receives radius of a circle and calculates its area  based on that radius, and returns the result.
-function calculateCircleArea(radius) {
-    let area = Math.PI * Math.pow(radius, 2);
-    return area;
-}
+// function calculateCircleArea(radius) {
+//     let area = Math.PI * Math.pow(radius, 2);
+//     return area;
+// }
 
+// prompt = require("prompt-sync")();
+// let circleRadius = Number(prompt("Enter the radius of the circle: "));
+// let circleArea = calculateCircleArea(circleRadius);
+// console.log("The area of the circle is: " + circleArea.toFixed(2)); 
+
+
+/*
+FUNCTION EXPRESSIONS     00000000000000000000000000000000000000000000000000000000000000000000
+Function expressions are a way to define functions in JavaScript. They allow you to create anonymous functions (functions without a name) and assign them to variables. This can be useful for creating functions on the fly, or for passing functions as arguments to other functions.
+Syntax:
+const functionName = function(parameters) {
+     code to be executed
+     return value; // optional
+};
+
+
+
+*/
+// let circleArea=function(r){
+//     return Math.PI * Math.pow(r, 2);
+// }
+// const prompt = require("prompt-sync")();
+// console.log("The area of the circle is: " + circleArea(radis));
+
+/* 
+Write a JS code that calculates the square of diffrent variables using function expression
+
+*/
+
+let square = function(num) {
+    return num * num;
+   
+}
 prompt = require("prompt-sync")();
-let circleRadius = Number(prompt("Enter the radius of the circle: "));
-let circleArea = calculateCircleArea(circleRadius);
-console.log("The area of the circle is: " + circleArea.toFixed(2)); 
+let number = Number(prompt("Enter a number to calculate its square: "));
+//let result = square(number);
+console.log("The square of " + number + " is: " + square(number));
