9b9888749aa624a66b3e852fd3be7adf71eecfb4
snyderphina-com <snyderphina@gmail.com>
2026-02-19 11:10:44 +0300

    .js

---
+++ recap.js
@@ -0,0 +1,15 @@
+// function cut (){
+// var k=5;
+// let j=7;
+// if(true);
+// var l=10;
+// console.log(k);
+// console.log(j);
+// console.log(l);
+// }
+// cut();
+
+// let x=0;
+// console.log(x++);
+// console.log(++x);
+
