25fcc9c550d33218358baf9ff45695165ca5624d
snyderphina-com <snyderphina@gmail.com>
2026-02-19 11:37:29 +0300

    revision

--- recap.js
+++ recap.js
@@ -13,3 +13,23 @@
 // console.log(x++);
 // console.log(++x);
 
+// let y=30;
+// if (true){
+//     let y=40;
+//     console.log(y);
+// }
+// // console.log(y);
+
+// let greet=(name)=>"Hello, "+name+"! Welcome to JavaScript functions.";
+// console.log(greet("alishia"));
+
+// let a=5;
+// function test(){
+   
+//     console.log(a);
+//      let a=10;
+// }
+// test();
+// console.log(a); 
+
+console.log
